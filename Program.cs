b03ad04184ee17ffb604197d20132f3ec4ead36e
cyril <cyril@haberer.fr>
2026-03-24 13:29:05 +0100

    jeu du nombre

--- Program.cs
+++ Program.cs
@@ -2,9 +2,41 @@ namespace PremierProgram
 {
     class Program
     {
-        static void Main(string[] arg)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello Words !");
+            Random rand = new Random();
+            int nombre_secret = rand.Next(1, 101);
+            int nb_essai = 0;
+            int propal;
+
+            Console.WriteLine("Je pense à un nombre entre 1 et 100 !");
+            Console.WriteLine("Essaie de le deviner :");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out propal))
+                {
+                    Console.WriteLine("Erreur : tu dois rentrer un nombre ! Essaie encore :");
+                    continue;
+                }
+
+                nb_essai++;
+
+                if (propal == nombre_secret)
+                {
+                    Console.WriteLine($"Bravo ! {nombre_secret} était le bon nombre ! Tu as trouvé en {nb_essai} essai(s).");
+                    break;
+                }
+                else if (propal > nombre_secret)
+                {
+                    Console.WriteLine($"{propal} est trop grand ! Essaie encore :");
+                }
+                else
+                {
+                    Console.WriteLine($"{propal} est trop petit ! Essaie encore :");
+                }
+            }
         }
     }
 }
